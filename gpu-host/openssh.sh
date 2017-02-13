469ffd1935a3812fccd80355cb1ff67a602f2748
Maxim Letunov <maxim.letunov@artezio.com>
2017-02-13 14:02:03 +0300

    remove ssh script from host

--- gpu-host/openssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get install -y sudo openssh-server && \
-   /etc/init.d/ssh start
-USER=test PASS=test
-useradd -d / -s /bin/bash -p $(openssl passwd -1 $PASS) -g sudo $USER
